d6209ad514ff0978abe5ecd24c7e11712cc075c3
originalT49 <87042682+pyschot49@users.noreply.github.com>
2021-09-23 03:02:26 +0000

    Initial commit

---
+++ nextjs-blog/__tests__/first_test.js
@@ -0,0 +1,12 @@
+
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Index from '../pages/index';
+
+describe("Index page", () => {
+    it("should render", () => {
+    render(<Index />);
+    const main = screen.getByRole("section");
+    expect(main).toBeInTheDocument();
+});
+});
---
+++ nextjs-blog/__tests__/testme.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Home from '../pages/index'
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    render(<Home />)
+
+    const heading = screen.getByRole("sections");
+    expect(heading).toBeInTheDocument()
+  })
+})
